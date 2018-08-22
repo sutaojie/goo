22c6ccfd71006c7a812b15ae2e82ccac69c4906b
sutaojie <sutaojie@gmail.com>
2018-08-22 13:35:47 +0800

    创建 buttongroup 上一页，下一页

---
+++ src/buttongroup.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "g-button-group"
+    }
+</script>
+
+<style scoped>
+
+</style>
