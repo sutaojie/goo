35997be7257a41bc1b1c8ec13666127403d85e0b
sutaojie <sutaojie@gmail.com>
2018-08-22 14:03:07 +0800

    给 buttongroup 组件添加样式  &:not(:first-child){margin-left: -1px;} 让边框隐藏  &:hover {z-index: 1;} 当 hover 后出现。

--- src/buttongroup.vue
+++ src/buttongroup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="g-button-group">
         <slot></slot>
     </div>
 </template>
@@ -10,6 +10,27 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+    .g-button-group{
+        display: inline-flex;
+        vertical-align:middle;
 
+        > .g-button{
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            &:hover {
+                z-index: 1;
+            }
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+        }
+    }
 </style>
