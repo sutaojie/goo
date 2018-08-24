41be6f9d4a8ba178a3fb75f55e7b8105290e9ae8
sutaojie <sutaojie@gmail.com>
2018-08-24 15:47:10 +0800

    创建 row 和 col 组件，网格布局基本雏形
    $class-prefix:col-;
            @for $n from 1 through 24 {
                &.#{$class-prefix}#{$n} {
                    width:($n /24) * 100%
                }
            }

---
+++ src/col.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        props:{
+            span:{
+                type:[Number, String]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .col{
+        background: #ccc;
+        border: 1px solid red;
+        width: 50%;
+        height: 20px;
+
+        $class-prefix:col-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                width:($n /24) * 100%
+            }
+        }
+    }
+
+</style>
