3be5d4a6bbfb86e01569dce86b45075c48d604d7
sutaojie <sutaojie@gmail.com>
2018-08-26 17:26:28 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -84,7 +84,12 @@
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg:rgba(0,0,0,0.75);
+    @keyframes fade-in{
+        0% { opacity: 0; transform: translate(-50%,100%)}
+        100% { opacity: 1; transform: translate(-50%,0%)}
+    }
     .toast{
+        animation: fade-in 1s;
         font-size: $font-size;min-height: $toast-min-height;line-height: 1.8;
         position:fixed;left:50%;
         display: flex;align-items:center;
