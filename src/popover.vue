ef802d6864b27ccf4e722d8a85fbce66597984d9
sutaojie <sutaojie@gmail.com>
2018-08-27 22:51:01 +0800

    解决当容器有 overflow hidden 造成的 bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,31 +1,37 @@
 <template>
-   <div class="popover" @click.stop="xxx">
-       <div class="content-wrapper" v-if="visible" @click.stop>
-           <slot name="content"></slot>
-       </div>
-       <slot></slot>
-   </div>
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" ref="contentWrapper" v-if="visible">
+            <slot name="content"></slot>
+        </div>
+        <span ref="triggerWrapper">
+            <slot></slot>
+       </span>
+    </div>
 </template>
 
 <script>
     export default {
         name: "popover",
-        data(){
+        data() {
             return {visible: false}
         },
-        methods:{
-            xxx(){
+        methods: {
+            xxx() {
                 this.visible = !this.visible
-                if(this.visible === true){
-                    this.$nextTick(()=>{
-                        let eventHandler = ()=>{
-                            this.visible =false
+                if (this.visible === true) {
+                    this.$nextTick(() => {
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+                        let eventHandler = () => {
+                            this.visible = false
                             console.log('document 隐藏 popover')
                             document.removeEventListener('click', eventHandler)
                         }
                         document.addEventListener('click', eventHandler)
                     })
-                }else{
+                } else {
                     console.log('vm 隐藏 popover')
                 }
             }
@@ -34,14 +40,14 @@
 </script>
 
 <style lang='scss' scoped>
-    .popover{
-        display:inline-flex;
+    .popover {
+        display: inline-flex;
         position: relative;
-        .content-wrapper{
-            position: absolute;
-            bottom:100%;
-            left: 0;
-            border: 1px solid red;
-        }
+    }
+
+    .content-wrapper {
+        position: absolute;
+        border: 1px solid red;
+        transform:translateY(-100%);
     }
 </style>
